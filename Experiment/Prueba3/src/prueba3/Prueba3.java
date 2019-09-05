f4b1d449e4c4217244f8f656360a5dd9dfc4a835
Carlos Manuel Sequí Sánchez <cmss_96@hotmail.com>
2019-09-05 20:12:53 +0200

    reached goal 1

--- Experiment/Prueba3/src/prueba3/Prueba3.java
+++ Experiment/Prueba3/src/prueba3/Prueba3.java
@@ -3,31 +3,56 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prueba2;
+package prueba3;
 
 // Para usar el clasificador HoeffdingTree directamente sin hacer
 // uso de la clase HT(que es lo mismo pero modificada para el uso de
 // restricciones monotónicas)
 //import moa.classifiers.trees.HoeffdingTree;
+import com.yahoo.labs.samoa.instances.Instance;
 import moa.classifiers.Classifier;
 import moa.core.TimingUtils;
 import moa.streams.generators.RandomRBFGenerator;
-import com.yahoo.labs.samoa.instances.Instance;
+//import weka.core.Instance;
+//import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import weka.core.converters.ArffLoader.ArffReader;
+import moa.streams.ArffFileStream;
 
+public class Prueba3 {
 
-public class Prueba2 {
-
-        public Prueba2(){
+        public Prueba3(){
         }
 
-        public void run(int numInstances, boolean isTesting){
+        public void run(/*int numInstances, boolean isTesting*/) throws FileNotFoundException, IOException{
             
                 // declaramos el clasificador que queremos utilizar
                 Classifier learner = new HT();
                 
+                // definimos el data set a leer
+                String nombreDataSet = "D:\\TFM-DataScience-DATCOM\\Experiment\\Data sets\\era\\era.arff";
+                
+                // inicializamos la cantidad de instancias a leer
+                int numInstances = 1000;
+                
+                // definimos el número de instancias por defecto
+               /* 
+                BufferedReader reader = new BufferedReader(new FileReader(nombreDataSet));
+                ArffReader arff = new ArffReader(reader, 1000);
+                Instances data = arff.getStructure();
+                data.setClassIndex(data.numAttributes()-1);
+                Instance inst;
+                while((inst=arff.readInstance(data)) != null){ data.add(inst);}
+                numInstances = data.numInstances();*/
+                //System.out.print(data);
+                
                 // declaramos el flujo de datos y lo dejamos listo para su uso
-                RandomRBFGenerator stream = new RandomRBFGenerator();
+                /*RandomRBFGenerator stream = new RandomRBFGenerator();
+                stream.prepareForUse();*/
+                ArffFileStream stream = new ArffFileStream(nombreDataSet,3);
                 stream.prepareForUse();
 
                 // establecemos la cabecera de los datos de streaming en el
@@ -47,19 +72,20 @@ public class Prueba2 {
                 while (stream.hasMoreInstances() && numberSamples < numInstances) {
                         // tomamos el siguiente dato
                         Instance trainInst = stream.nextInstance().getData();
-                        
+                        //Instance trainInst = (Instance) data.get(numberSamples);
+                        //System.out.print(trainInst+"\n");
                         // FASE TEST
-                        if (isTesting) {
+                        //if (isTesting) {
                                 // si lo clasifica de forma correcta sumamos
                                 // un acierto a las estadisticas
                                 if (learner.correctlyClassifies(trainInst)){
                                         numberSamplesCorrect++;
                                 }
-                        }
+                        //}
                         numberSamples++;
                         
                         // FASE TRAIN
-                        learner.trainOnInstance(trainInst);
+                        learner.trainOnInstance((com.yahoo.labs.samoa.instances.Instance)trainInst);
                 }
                 double accuracy = 100.0 * (double) numberSamplesCorrect/ (double) numberSamples;
                 double time = TimingUtils.nanoTimeToSeconds(TimingUtils.getNanoCPUTimeOfCurrentThread()- evaluateStartTime);
@@ -67,7 +93,7 @@ public class Prueba2 {
         }
 
         public static void main(String[] args) throws IOException {
-                Prueba2 exp = new Prueba2();
-                exp.run(1000, true);
+                Prueba3 exp = new Prueba3();
+                exp.run(/*1000, true*/);
         }
 }
