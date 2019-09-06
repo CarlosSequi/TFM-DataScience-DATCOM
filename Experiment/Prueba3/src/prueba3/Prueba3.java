1ec6b0e4fce2c0b93391be57b51ae35f625de008
Carlos Manuel Sequí Sánchez <cmss_96@hotmail.com>
2019-09-06 16:00:29 +0200

    MAE done

--- Experiment/Prueba3/src/prueba3/Prueba3.java
+++ Experiment/Prueba3/src/prueba3/Prueba3.java
@@ -16,9 +16,12 @@ import moa.streams.generators.RandomRBFGenerator;
 //import weka.core.Instance;
 //import weka.core.Instances;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import static java.lang.Math.abs;
 import weka.core.converters.ArffLoader.ArffReader;
 import moa.streams.ArffFileStream;
 import weka.core.Utils;
@@ -28,63 +31,114 @@ public class Prueba3 {
         public Prueba3(){
         }
 
-        public void run(/*int numInstances, boolean isTesting*/) throws FileNotFoundException, IOException{
+        public double runHT(String train, String test, int numAtributos) throws FileNotFoundException, IOException{
             
                 // declaramos el clasificador que queremos utilizar
                 Classifier learner = new HT();
                 
-                // definimos el data set a leer
-                String nombreDataSet = "D:\\TFM-DataScience-DATCOM\\Experiment\\Data sets\\era\\era.arff";
+                // declaramos un flujo de datos para train y otro para test
+                ArffFileStream trainStream = new ArffFileStream(train,numAtributos);
+                ArffFileStream testStream = new ArffFileStream(test,numAtributos);
+                trainStream.prepareForUse();
+                testStream.prepareForUse();
                 
-                // inicializamos la cantidad de instancias a leer
-                int numInstances = 1000;
-                
-                
-                // declaramos el flujo de datos y lo dejamos listo para su uso
-                ArffFileStream stream = new ArffFileStream(nombreDataSet,3);
-                stream.prepareForUse();
-
                 // establecemos la cabecera de los datos de streaming en el
                 // contexto del clasificador, para dejarlo listo para su uso
-                learner.setModelContext(stream.getHeader());
+                learner.setModelContext(trainStream.getHeader());
                 learner.prepareForUse();
-
-                int numberSamplesCorrect = 0; // cantidad de aciertos
-                int numberSamples = 0; // cantidad de muestras
                 
-                // para la toma de tiempo de cómputo
-                boolean preciseCPUTiming = TimingUtils.enablePreciseTiming();
-                long evaluateStartTime = TimingUtils.getNanoCPUTimeOfCurrentThread();
-                
-                // leemos datos del flujo mientras haya y aplicamos
-                // test-then-train sobre cada dato
-                while (stream.hasMoreInstances() && numberSamples < numInstances) {
-                        // tomamos el siguiente dato
-                        Instance trainInst = stream.nextInstance().getData();
-
-                        // si lo clasifica de forma correcta sumamos
-                        // un acierto a las estadisticas
-                        if (learner.correctlyClassifies(trainInst)){
-                            numberSamplesCorrect++;
-                        }
-                        else
-                        {
-                            System.out.print(Utils.maxIndex(learner.getVotesForInstance(trainInst)) + "\n");
-                            System.out.print(trainInst.classValue()+ "\n");
-                            System.out.print("---------------------\n");
-                        }
-                        numberSamples++;
-                        
-                        // FASE TRAIN
-                        learner.trainOnInstance((com.yahoo.labs.samoa.instances.Instance)trainInst);
+                //--------------------------------------------------------------
+                // TRAIN
+                // primero entrenamos con los datos de train
+                while (trainStream.hasMoreInstances()) {
+                    // tomamos el siguiente dato
+                    Instance trainInst = trainStream.nextInstance().getData();
+                    // aprendemos de él
+                    learner.trainOnInstance((com.yahoo.labs.samoa.instances.Instance)trainInst);
                 }
-                double accuracy = 100.0 * (double) numberSamplesCorrect/ (double) numberSamples;
-                double time = TimingUtils.nanoTimeToSeconds(TimingUtils.getNanoCPUTimeOfCurrentThread()- evaluateStartTime);
-                System.out.println(numberSamples + " instances processed with " + accuracy + "% accuracy in "+time+" seconds.");
+                
+                //--------------------------------------------------------------
+                // TEST
+                // Preparamos ahora el learner para el testStream
+                learner.setModelContext(testStream.getHeader());
+                learner.prepareForUse();
+                
+                // almacenamos la suma de errores en test
+                double sumOfErrors = 0;
+                // creamos el contador de instancias del data set
+                int cantidadInstanciasTest = 0;
+                // contamos la cantidad de aciertos realizados
+                int cantidadAciertos = 0;
+                // Validamos con el modelo creado anteriormente 
+                // y almacenamos los errores cometidos para poder calcular
+                // MAE posteriormente
+                while (testStream.hasMoreInstances())  {
+                    // sumamos la nueva instancia al contador de instancias
+                    cantidadInstanciasTest++;
+                    // tomamos el siguiente dato
+                    Instance testInst = testStream.nextInstance().getData();
+                    //System.out.print(testInst+"\n"+testInst.classValue()+"\n");
+                    // sumamos los errores haciendo la diferencia entre ellos
+                    if(!learner.correctlyClassifies(testInst))
+                    {
+                        
+                        // sumamos el error cometido para obtener MAE
+                        double error = abs(Utils.maxIndex(learner.getVotesForInstance(testInst)) - (testInst.classValue()));
+                        sumOfErrors += error;
+                        
+                        // PARA HACER COMPROBACIONES
+                        /*
+                        System.out.print("\n------\n");
+                        System.out.print("Instancia numero: "+cantidadInstanciasTest+" --> "+testInst);
+                        System.out.print("\nIndice de clase predicho: "+Utils.maxIndex(learner.getVotesForInstance(testInst)) + "\nIndice de clase real:" + testInst.classValue());
+                        */
+                    }
+                    else
+                    {
+                        cantidadAciertos++;
+                        
+                        // PARA HACER COMPROBACIONES:
+                        // el predictor me da el índice del vector de posibles valores de clase(C) que piensa que
+                        // se corresponde con el valor que ha de tener la clase de la instancia predicha.
+                        // si c = {3,4,5} (3, 4 y 5 son las distintas clases que existen)
+                        // y el predictor me devuelve el valor 2 para una instancia x, entonces es porque
+                        // piensa que la etiqueta que ha de tener x es 5 (c[2])
+                        /*
+                        System.out.print("\n------\n");
+                        System.out.print("Instancia numero: "+cantidadInstanciasTest+" --> "+testInst);
+                        System.out.print("\nIndice de clase predicho: "+Utils.maxIndex(learner.getVotesForInstance(testInst)) + "\nIndice de clase real:" + testInst.classValue());
+                        */
+                    }
+                }
+                System.out.print("\nCantidad de aciertos: " + cantidadAciertos + "/" + cantidadInstanciasTest);
+                double MAE = sumOfErrors/cantidadInstanciasTest;
+                
+                return MAE;
+        }
+        
+        public double HTCrossValidation(String dataset,int numAtributos, int numFolds) throws IOException
+        {
+            // Almacenamos la suma de resultados de cada partición
+            double sumOfAccuracies = 0;
+            for(int i=1; i <= numFolds; i++)
+            {
+                // declaramos los conjuntos de train y de test
+                String train = "D:\\TFM-DataScience-DATCOM\\Experiment\\Data sets\\"+ dataset +"\\"+ dataset +"-10-"+i+"tra.dat";
+                String test = "D:\\TFM-DataScience-DATCOM\\Experiment\\Data sets\\"+ dataset +"\\"+ dataset +"-10-"+i+"tst.dat";
+                double MAE = runHT(train, test, numAtributos);
+                //System.out.print(MAE);
+                sumOfAccuracies += MAE;
+            }
+            
+            return sumOfAccuracies / numFolds;      
         }
 
         public static void main(String[] args) throws IOException {
                 Prueba3 exp = new Prueba3();
-                exp.run(/*1000, true*/);
+                // indicamos el data set a usar, la cantidad de atributos
+                // (incluyendo al de clase) y el número de folds de la
+                // cross validation
+                double result = exp.HTCrossValidation("swd",10,10);
+                System.out.print("\n-----------\n10 fold CV MAE = "+result);
         }
 }
